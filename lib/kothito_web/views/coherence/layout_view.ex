be9a01fd69fe224925e0cde90846fd4add1282aa
Zahidul Haque <iisowrovii@gmail.com>
2017-12-15 17:05:31 +0600

    fix flash message display for authentication layout

--- lib/kothito_web/views/coherence/layout_view.ex
+++ lib/kothito_web/views/coherence/layout_view.ex
@@ -1,3 +1,19 @@
 defmodule KothitoWeb.Coherence.LayoutView do
   use KothitoWeb.Coherence, :view
+
+  def show_flash(conn) do
+    conn |> get_flash |> flash_msg
+  end
+
+  def flash_msg(%{"info" => msg}) do
+    ~E"<div class='alert alert-info alert-icon-left'><%= msg %></div>"
+  end
+
+  def flash_msg(%{"error" => msg}) do
+    ~E"<div class='alert alert-danger alert-icon-left'><%= msg %></div>"
+  end
+
+  def flash_msg(_) do
+    nil
+  end
 end
