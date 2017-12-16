b8b2f58cd07f73df43a93fefc77c2187b03ccdee
Zahidul Haque <iisowrovii@gmail.com>
2017-12-17 02:11:26 +0600

    add feather icons

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -7,21 +7,6 @@ exports.config = {
         "js/vendor.js": /^(vendor|deps|node_modules).*/,
         "js/auth.js": /^js\/auth/
       }
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
     },
     stylesheets: {
       joinTo: "css/app.css",
@@ -35,17 +20,12 @@ exports.config = {
   },
 
   conventions: {
-    // This option sets where we should place non-css and non-js assets in.
-    // By default, we set this to "/assets/static". Files in this directory
-    // will be copied to `paths.public`, which is "priv/static" by default.
     assets: /^(static)/
   },
 
   // Phoenix paths configuration
   paths: {
-    // Dependencies and current project directories to watch
     watched: ["static", "css", "js", "vendor"],
-    // Where to compile files to
     public: "../priv/static"
   },
 
