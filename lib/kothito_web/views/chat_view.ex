c80aff34af59fb698aee048bad6616d501c475ef
Zahidul Haque <iisowrovii@gmail.com>
2017-12-26 13:40:56 +0600

    display user profile pic & name

--- lib/kothito_web/views/chat_view.ex
+++ lib/kothito_web/views/chat_view.ex
@@ -2,4 +2,13 @@ defmodule KothitoWeb.ChatView do
   use KothitoWeb, :view
 
   def page(_), do: "chat-application"
+
+  def avatar(user, version \\ :small) do
+    Kothito.AvatarUploader.url({user.avatar, user}, version)
+    |> img_tag
+  end
+
+  def name(user) do
+    user.firstname <> " " <> user.lastname
+  end
 end
