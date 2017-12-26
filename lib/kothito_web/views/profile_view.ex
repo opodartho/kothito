2ca6b384820d17bad209f3e7052c263bb1d42bc1
Zahidul Haque <iisowrovii@gmail.com>
2017-12-26 21:52:36 +0600

    fix users page

--- lib/kothito_web/views/profile_view.ex
+++ lib/kothito_web/views/profile_view.ex
@@ -1,3 +1,15 @@
 defmodule KothitoWeb.ProfileView do
   use KothitoWeb, :view
+
+  def fullname(user) do
+    try do
+      user.firstname <> " " <> user.lastname
+    rescue
+      _-> user.username
+    end
+  end
+
+  def avatar(user, version) do
+    Kothito.AvatarUploader.url({user.avatar, user}, version)
+  end
 end
