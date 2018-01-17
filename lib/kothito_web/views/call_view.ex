c2db06244d1461e5bc50e23483c769968dd7fae8
Zahidul Haque <iisowrovii@gmail.com>
2018-01-17 12:05:52 +0600

    finish design incoming call modal

--- lib/kothito_web/views/call_view.ex
+++ lib/kothito_web/views/call_view.ex
@@ -3,4 +3,27 @@ defmodule KothitoWeb.CallView do
 
   def header(_), do: nil
   def page(_), do: "call-incoming"
+
+  def avatar_url(user, version \\ :small) do
+    Kothito.AvatarUploader.url({user.avatar, user}, version)
+  end
+
+  def caller(conn, room) do
+    room.users
+    |> Enum.reject(fn(e) -> e.id === current_user(conn).id end)
+    |> List.first
+  end
+
+  def caller_name(conn, room) do
+    user = caller(conn, room)
+    fullname(user)
+  end
+
+  def fullname(user) do
+    try do
+      user.firstname <> " " <> user.lastname
+    rescue
+      _ -> user.username
+    end
+  end
 end
